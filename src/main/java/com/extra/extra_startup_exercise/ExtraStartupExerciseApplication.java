64f56cba9ce052053c8c6cdd99bffb5d285164c9
YehielSiri <yehiels14@gmail.com>
2024-12-31 10:59:05 +0200

    first commit

---
+++ src/main/java/com/extra/extra_startup_exercise/ExtraStartupExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.extra.extra_startup_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraStartupExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraStartupExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/extra/extra_startup_exercise/ExtraStartupExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extra.extra_startup_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraStartupExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
