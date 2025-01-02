03f2b930cc9c586e4ef6634004d0b4f99748f908
YehielSiri <yehiels14@gmail.com>
2025-01-02 10:24:03 +0200

    userAccount class

--- src/main/java/com/extra_startup_exercise/Main.java
+++ src/main/java/com/extra_startup_exercise/Main.java
@@ -1,2 +1,34 @@
-package com.extra_startup_exercise;public class Main {
+package com.extra_startup_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.Clock;
+import java.util.List;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        return new GreetResponse(
+                "Hello",
+                List.of("Java", "Python", "C"),
+                new Person("Shmuel"));
+    }
+
+    record Person(String name){
+
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person) {
+
+    }
 }
---
+++ src/main/java/com/extra_startup_exercise/UserAccount.java
@@ -0,0 +1,89 @@
+package com.extra_startup_exercise;
+
+import java.util.Objects;
+
+public class UserAccount {
+//    private Integer id;
+    private String firstName = "";
+    private String surename = "";
+    private String userName = "";    // Add security validation - username has to be unique!
+    private String password = "";
+
+    public UserAccount(String firstName,
+                       String surename,
+                       String userName,
+                       String password) {
+        // Validate fields are not null
+        if (firstName != null ||
+                surename != null ||
+                userName != null ||
+                password != null) {
+            this.firstName = firstName;
+            this.surename = surename;
+            this.userName = userName;
+            this.password = password;
+        }
+    }
+
+    public UserAccount() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public void setSurename(String surename) {
+        this.surename = surename;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserAccount that = (UserAccount) o;
+        return getFirstName().equals(that.getFirstName()) &&
+                getSurename().equals(that.getSurename()) &&
+                getUserName().equals(that.getUserName()) &&
+                getPassword().equals(that.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getSurename(), getUserName(), getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return "UserAccount{" +
+                "firstName='" + firstName + '\'' +
+                ", surename='" + surename + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
