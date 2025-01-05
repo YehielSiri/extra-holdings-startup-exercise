ab1478f132d8363aa6fd947c8b77de22125442e2
YehielSiri <yehiels14@gmail.com>
2025-01-05 14:21:40 +0200

    connecting REST methods to the database

--- src/main/java/com/extra_startup_exercise/Main.java
+++ src/main/java/com/extra_startup_exercise/Main.java
@@ -3,32 +3,46 @@ package com.extra_startup_exercise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Clock;
 import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("api/v1/user_accounts")
 public class Main {
+
+    private final UserAccountRepository userAccountRepository;
+
+    public Main(UserAccountRepository userAccountRepository) {
+        this.userAccountRepository = userAccountRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        return new GreetResponse(
-                "Hello",
-                List.of("Java", "Python", "C"),
-                new Person("Shmuel"));
+    @GetMapping
+    public List<UserAccount> getUserAccounts() {
+        return userAccountRepository.findAll();
     }
-
-    record Person(String name){
-
-    }
-
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person) {
-
-    }
+//    @GetMapping("/greet")
+//    public GreetResponse greet() {
+//        return new GreetResponse(
+//                "Hello",
+//                List.of("Java", "Python", "C"),
+//                new Person("Shmuel"));
+//    }
+//
+//    record Person(String name){
+//
+//    }
+//
+//    record GreetResponse(
+//            String greet,
+//            List<String> favProgrammingLanguages,
+//            Person person) {
+//
+//    }
 }
