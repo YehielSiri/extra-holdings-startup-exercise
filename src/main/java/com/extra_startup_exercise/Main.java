80592eb8bf5b790ae99627f0ca0a083f6cfdc137
YehielSiri <yehiels14@gmail.com>
2025-01-06 13:12:54 +0200

    App Ordered complete include Main.java

--- src/main/java/com/extra_startup_exercise/Main.java
+++ src/main/java/com/extra_startup_exercise/Main.java
@@ -1,24 +1,13 @@
 package com.extra_startup_exercise;
 
-import com.extra_startup_exercise.entity.UserAccount;
-import com.extra_startup_exercise.repository.UserAccountRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+//import com.extra_startup_exercise.entity.UserAccount;
+//import com.extra_startup_exercise.repository.UserAccountRepository;
+//import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("api/v1/userAccounts")
 public class Main {
-
-    private final UserAccountRepository userAccountRepository;
-
-    public Main(UserAccountRepository userAccountRepository) {
-        this.userAccountRepository = userAccountRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
