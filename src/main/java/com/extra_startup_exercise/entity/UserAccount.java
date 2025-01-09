cd0065687a03be94829b0d76e4d20e6e1fb3355f
YehielSiri <yehiels14@gmail.com>
2025-01-09 10:12:25 +0200

    A step back for solving the Swagger error with Cristian. I have disabled all the late additions: Lombok, Infinispian cache, Hibernate validation

--- src/main/java/com/extra_startup_exercise/entity/UserAccount.java
+++ src/main/java/com/extra_startup_exercise/entity/UserAccount.java
@@ -1,7 +1,13 @@
 package com.extra_startup_exercise.entity;
 
 import jakarta.persistence.*;
+
 import java.util.Objects;
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.Size;
 
 @Entity
 public class UserAccount {
@@ -15,10 +21,22 @@ public class UserAccount {
         strategy = GenerationType.SEQUENCE,
             generator = "userAccount_id_sequence"
     )
+//    @NotNull(message = "Enter a valid UserAccount Id")
     private Integer id;   // Will get a primary key from JPA
+
+//    @NotBlank(message = "User Account first name can't be left empty")
     private String firstName = "";
+
+//    @NotBlank(message = "User Account last name can't be left empty")
     private String surename = "";
+
+    //    @Email(message = "Please enter a valid email Id")
+//    @NotNull(message = "Email can not be NULL")
     private String userName = "";    // Add security validation - username has to be unique!
+
+//    @NotBlank(message = "employee name can't be left empty")
+//    @size(min = 8, max = 8,
+//            message = "Password should have a length of 8 characters.")
     private String password = "";
 
     public UserAccount(String firstName,
